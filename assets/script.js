ed1c4afcf13de5753e7a93ba00c3b80a36cd53cb
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-02-13 23:16:19 -0500

    Re-phrased the questions

--- assets/script.js
+++ assets/script.js
@@ -12,20 +12,22 @@ var generateBtn = document.querySelector("#generate");
 function random (data){
     return data[Math.floor(Math.random() * data.length)]
 }
+
 function generatePassword(){
     var password= ""
     var passwordOptions = ""
 
-    var passwordLength = parseInt(prompt("How long do you want your password to be?"))
+    var passwordLength = parseInt(prompt("How many characters do you want your password to be?"))
     console.log(passwordLength)
     while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-        passwordLength = parseInt(prompt("How long do you want your password to be?"))
+        passwordLength = parseInt(prompt("Please choose a number between 8 - 128 "))
     }
 
-    var includeNumber = confirm("Do you want your password to include numbers?")
-    var includeSymbol = confirm("Do you want your password to include symbols?")
-    var includeAlphabetLower = confirm("Do you want your password to include lower case letters?")
-    var includeAlphabetCap = confirm("Do you want your password to include upper case letters?")
+    var includeNumber = confirm("If you want your password to include numbers press OK")
+    var includeSymbol = confirm("If you want your password to include symbols press OK")
+    var includeAlphabetLower = confirm("If you want your password to include lower case letters press OK")
+    var includeAlphabetCap = confirm("If you want your password to include upper case letters press OK")
+
 
 
     if(includeNumber){
@@ -52,6 +54,8 @@ function generatePassword(){
         console.log(passwordOptions, password)
     }
 
+    
+
     for (let i = password.length; i < passwordLength; i++) {
         password += random(passwordOptions)        
     }
@@ -59,7 +63,7 @@ function generatePassword(){
     return password
 
 }
-
+ 
 
 // Write password to the #password input
 function writePassword() {
